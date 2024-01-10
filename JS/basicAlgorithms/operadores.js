f1f38d4b4296c7bcc11d221356a1e5f3786fd61d
Monique Mendes <moniiquemendes96@gmail.com>
2024-01-10 09:13:31 +0100

    operadores ejer

---
+++ JS/basicAlgorithms/operadores.js
@@ -0,0 +1,30 @@
+/* 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+1.2 Divide 10 por 2 y muestra el resultado en un console.
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5. 
+*/
+
+//!---------------------------------------------------------------------------------
+//? 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+//!---------------------------------------------------------------------------------
+
+let multiplica = 10 * 5;
+//console.log(multiplica);
+
+//!---------------------------------------------------------------------------------
+//? 1.2 Divide 10 por 2 y muestra el resultado en un console
+//!---------------------------------------------------------------------------------
+
+let dividir = 10 / 2;
+//console.log(dividir);
+
+//!---------------------------------------------------------------------------------
+//?  1.3 Muestra mediante un console el resto de dividir 15 por 9.
+//!---------------------------------------------------------------------------------
+
+let resto = 15 % 9;
+//
+console.log(resto);
