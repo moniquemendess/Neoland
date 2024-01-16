a8527fe43968f7e05b57d941b75ef594b368165a
Monique Mendes <moniiquemendes96@gmail.com>
2024-01-16 16:10:05 +0100

    Iteración #7: For...of avanzado

--- JS/loops/app.js
+++ JS/loops/app.js
@@ -87,7 +87,7 @@ console.log(placesArray); */
 //? Iteración #6: Probando For
 //!---------------------------------------------------------------------------------------------------------------------------------------------------------
 
-const toys = [
+/*const toys = [
   { id: 5, name: "Buzz MyYear" },
   { id: 11, name: "Action Woman" },
   { id: 23, name: "Barbie Man" },
@@ -102,4 +102,26 @@ for (const juguetes of toys) {
     toysArray.push(juguetes);
   }
 }
-console.log(toysArray);
+console.log(toysArray); */
+
+//!---------------------------------------------------------------------------------------------------------------------------------------------------------
+//? Iteración #7: For...of avanzado
+//!---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const toyss = [
+  { id: 5, name: "Buzz MyYear", sellCount: 10 },
+  { id: 11, name: "Action Woman", sellCount: 24 },
+  { id: 23, name: "Barbie Man", sellCount: 15 },
+  { id: 40, name: "El gato con Guantes", sellCount: 8 },
+  { id: 40, name: "El gato felix", sellCount: 35 },
+];
+
+const popularToys = [];
+
+for (const toysArray of toyss) {
+  if (toysArray.sellCount > 15) {
+    popularToys.push(toysArray);
+  }
+}
+
+console.log(popularToys);
