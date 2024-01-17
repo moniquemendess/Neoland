b503e539b0ea6a4393e3107e27c17212859240a0
Monique Mendes <moniiquemendes96@gmail.com>
2024-01-17 17:20:37 +0100

    Iteración #3 3.1, 3.2, 3.3, 3.4

--- JS/ES6/app.js
+++ JS/ES6/app.js
@@ -87,11 +87,66 @@ console.log("🚀 ~ name:", name); */
 //? y comprueba que todo esta bien imprimiendolo.
 //!-----------------------------------------------------------------------------------------------------------------------
 
-const car = { name: "Mazda 6", itv: [2015, 2011, 2020] };
+/* const car = { name: "Mazda 6", itv: [2015, 2011, 2020] };
 
-const { name, itv } = car;
+const { name, itv } = car; // crie duas variables do array car 
 
-const [ano01, ano02, ano03] = itv;
+const [ano01, ano02, ano03] = itv; // crie 3 variables del itv con los valores de 2015, 2011 y 2020
+
 console.log("🚀 ~ ano03:", ano03);
 console.log("🚀 ~ ano02:", ano02);
-console.log("🚀 ~ ano01:", ano01);
+console.log("🚀 ~ ano01:", ano01); */
+
+//!---------------------------------------------------------------------------------------------------------------------
+//? Iteración #3: Spread Operator
+//? 3.1 Dado el siguiente array, crea una copia usando spread operators.
+//!-----------------------------------------------------------------------------------------------------------------------
+
+/* const pointsList = [32, 54, 21, 64, 75, 43];
+
+const pointsListCopy = [...pointsList]; // hecho una copia 
+console.log("🚀 ~ pointsListCopy:", pointsListCopy); */
+
+//!---------------------------------------------------------------------------------------------------------------------
+//? Iteración #3: Spread Operator
+//? 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+//!-----------------------------------------------------------------------------------------------------------------------
+
+/* const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+
+const toyCopy = { ...toy }; // hecho una referencia al array original 
+console.log("🚀 ~ toyCopy:", toyCopy); */ // La copia me sale todo el array toy
+
+//!---------------------------------------------------------------------------------------------------------------------
+//? Iteración #3: Spread Operator
+//? 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando spread operatos.
+//!-----------------------------------------------------------------------------------------------------------------------
+
+/*const pointsList = [32, 54, 21, 64, 75, 43];
+const pointsLis2 = [54, 87, 99, 65, 32];
+
+const point2Copy = [...pointsList, ...pointsLis2]; // hecho una copia y dentro del point2Copy tiene 2 array juntos
+console.log("🚀 ~ point2Copy:", point2Copy); */
+
+//!---------------------------------------------------------------------------------------------------------------------
+//? Iteración #3: Spread Operator
+//? 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos con spread operator
+//!-----------------------------------------------------------------------------------------------------------------------
+
+/* const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+const toyUpdate = { lights: "rgb", power: ["Volar like a dragon", "MoonWalk"] };
+
+const toyUpdateCopy = { ...toy, ...toyUpdate };
+console.log("🚀 ~ toyUpdateCopy:", toyUpdateCopy); */
+
+//!---------------------------------------------------------------------------------------------------------------------
+//? Iteración #3: Spread Operator
+//? 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 pero sin editar el array inicial. De nuevo,
+//? pero sin editar el array inicial. De nuevo, usando spread operatos.
+//!-----------------------------------------------------------------------------------------------------------------------
+
+const colors = ["rojo", "azul", "amarillo", "verde", "naranja"];
+
+const colorCopy = [...colors];
+colorCopy.splice(2, 1);
+console.log("🚀 ~ colorCopy:", colorCopy);
